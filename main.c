73939cc16a3a2f142a2a9c755c2b61e88bab4689
siribins <144154845+siribins@users.noreply.github.com>
2024-04-18 20:23:30 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int len=0;
+struct Item
+{
+    char name[50];
+    int quantity;
+    float price;
+};
 
  int main()
  {
-    char mainname[50];
-    int num=0;
-    char email[50];
-    cusname(mainname);
-    mobnum(num);
-    cusemail(email);
+    printf("LOGIN\n");
+    
+    char user[50];
+    char password[50];
+
+    username(user);
+    pass(password);
+
+    int item_num=0;
+    struct Item *items = (struct Item* )malloc(100 * sizeof(struct Item));
+
+    char customer_name[50];
+    int customer_number=0;
+
+    strcpy(customer_name, user);
+    customer_number=mobnum(customer_number);
 
     int choice=0;
-    printf("How many items? ");
-    scanf("%d",&len);
-    char name[len][50];
-    int quantity[len];
-    float price[len];
-
     while (choice!=3)
     {
-        printf("\n1. Item Entry \n2. Generate Bill \n3. Quit(3)\nSelect an option:");
+        printf("\n1. Item Entry \n2. Generate Bill \n3. Quit\nSelect an option:");
         scanf("%d",&choice);
         printf("\n");
 
         switch (choice)
         {
             case 1:
-                for (int i=0; i<len; i++)
-                {
-                    itemname(name[i]);
-                    itemquantity(&quantity[i]);
-                    itemprice(&price[i]);
-                }
+                itemname(items[item_num].name);
+                items[item_num].quantity=itemquantity(&items[item_num].quantity);
+                items[item_num].price=itemprice(&items[item_num].price);
+                item_num++;
                 break;
+
             case 2: 
-                printf("Customer name: %s\n",mainname);
-                printf("Customer mobile number: %d\n",num);
-                printf("Customer email: %s\n",email);
-                GenBill(name, quantity, price, len);                
+                GenBill(customer_name, customer_number, items, item_num);  
                 break;
+
             case 3:
                 printf("Thank you");
                 break;
             default:
                 printf("Invalid Entry");
-                break;
         }
     }
+    free(items);
+    return 0;
  }
