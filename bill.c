4568980ef03d90bc79b35225c38aa3050e411ddf
siribins <144154845+siribins@users.noreply.github.com>
2024-04-18 20:22:39 +0530

    Update bill.c

--- bill.c
+++ bill.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 
-void GenBill(char name[][50], int quantity[], float price[], int len)
+struct Item
 {
-    printf("NO  ITEM    QUANTITY    PRICE   TOTAL\n");
-    for (int i = 0; i < len; i++)
-    {
-        printf("%d   %s     %d          %.2f     %.2f\n", i+1, name[i], quantity[i], price[i], (quantity[i] * price[i]));
+    char name[50];
+    int quantity;
+    float price;
+};
+
+void GenBill(char *name, int number, struct Item items[], int item_num)
+{
+    printf("\n");
+    printf("**********************************************************\n");
+    printf("Customer name: %s\n", name);
+    printf("Customer number: %d\n", number);
+    printf("**********************************************************\n");
+
+    printf("NO\tITEM\t\tQUANTITY\tPRICE\tITEM TOTAL\n");
+    float sum=0;
+    for (int i = 0; i < item_num; i++)
+    {  
+        int k=i;
+        float total=0;
+        total=(items[i].quantity)*(items[i].price);
+        printf("%d\t%s\t\t%d\t\t%.2f\t%.2f\n", k+1, items[i].name, items[i].quantity, items[i].price, total);
+        sum+=total;
     }
+
+    printf("**********************************************************\n");
+
+    printf("Total: %.2f\t\t\t\t GST:8%%\n", sum);
+    printf("Total with GST: %.2f\n", sum+(sum*0.08));
+
+    printf("**********************************************************\n");
+
 }
